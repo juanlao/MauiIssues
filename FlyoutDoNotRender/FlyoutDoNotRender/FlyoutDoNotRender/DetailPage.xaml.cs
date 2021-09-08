23e69c30d34b08e4b7fae36de17ae70bdcb90f6a
Juan María Laó Ramos <jlao@plainconcepts.com>
2021-09-08 16:58:24 +0200

    Flyout does not render on mac neither ios

---
+++ FlyoutDoNotRender/FlyoutDoNotRender/FlyoutDoNotRender/DetailPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace FlyoutDoNotRender
+{
+	public partial class DetailPage
+	{
+		public DetailPage()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ FlyoutDoNotRender/FlyoutDoNotRender/FlyoutDoNotRender/MenuPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace FlyoutDoNotRender
+{
+	public partial class MenuPage
+	{
+		public MenuPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
